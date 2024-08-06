7f3ddd83d586c8e852ba0db7fbcd3999b83239fd
olkaniecki <olivialkaniecki@gmail.com>
2024-08-06 16:41:20 -0400

    Collision wall system decided

--- game.py
+++ game.py
@@ -1,19 +1,35 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((720, 950))
+width, height = 600, 680
+screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
-running = True
 
+size = 40
+pygame.display.set_caption("Pacman-Plus")
+
+# Colors:
+BLACK = (0, 0 , 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+collision_walls = []
+
+for row in range(width):
+    collision_walls.append(pygame.Rect(row, 0, 20, 20))
+
+
+
+running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill("black")
+    screen.fill(BLACK)
 
-    # RENDER YOUR GAME HERE
-
+    for wall in walls:
+        pygame.draw.rect(screen, BLUE, wall)
 
     pygame.display.flip()
 
